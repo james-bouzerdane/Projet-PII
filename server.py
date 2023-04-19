f224cd0379d1566e9ab77e289c007ab22a3d29bb
james-bouzerdane <129988956+james-bouzerdane@users.noreply.github.com>
2023-04-19 18:32:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
                 '/hello_world.html': 'hello_world.html',
                 '/test.html': 'test.html',
                 '/tony.html': 'tony.html'}
-else:
+else: # Let the user select the dictionary (the pages that the webserver will show depending of the url)
     print("Please select the text file containing the json dictionary that consists of:\n"
           "The url (key) and the full path of the corresponding HTML file (value)")
     file_dict = askopenfilename()
@@ -14,22 +14,22 @@ else:
     with open(file_dict, 'r') as f:
         dict_url = json.load(f)
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 8000))
-server_socket.listen()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create socket
+server_socket.bind(('localhost', 8000)) # Bind the socket to localhost at port 8000
+server_socket.listen() # Listening for connections
 
 while True:
     print('Listening for incoming connections...')
     client_socket, client_address = server_socket.accept()
     print(f'Accepted connection from {client_address[0]}:{client_address[1]}')
-    request_data = client_socket.recv(1024)
+    request_data = client_socket.recv(1024) # Reads a maximum of 1024 bytes
     request_lines = request_data.decode().split('\n')
     request_method, request_url, request_protocol = request_lines[0].split()
 
     if request_url[1:] != 'favicon.ico':
         try:
             file_name = dict_url[request_url]
-        except KeyError:
+        except KeyError: # url not recognized by webserver
             file_name = '404.html'
         with open(file_name, 'r') as file:
             file_content = file.read()
@@ -38,5 +38,5 @@ while True:
             else:
                 response = 'HTTP/1.0 200 OK\n\n' + file_content
 
-        client_socket.sendall(response.encode())
+        client_socket.sendall(response.encode()) #Show HTML page on browser
     client_socket.close()
